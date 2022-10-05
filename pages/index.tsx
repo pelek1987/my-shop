a5ddae726071ab722faba602e8fcabf03a882689
Pawel <pawel.sioda@gmail.com>
2022-10-05 23:17:28 +0200

    add grid to main content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,14 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow max-w-lg mx-auto w-full  px-4 py-2 bg-teal-100">
-        Właściwa zawartość
+      <main className="flex-grow max-w-lg mx-auto grid sm:grid-cols-2 gap-6 p-6">
+        <img src="https://picsum.photos/536/364" alt="random photo" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+          exercitationem libero molestias nemo, magni debitis. Suscipit fugit
+          numquam, sed dolorem ab, voluptatem repellendus voluptate vero dolorum
+          accusantium pariatur impedit nostrum.
+        </p>
       </main>
       <Footer />
     </div>
