365a1656f507e07014745d08164feafbe26af2b5
Pawel <pawel.sioda@gmail.com>
2022-10-05 23:06:08 +0200

    create basic structure of app

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="bg-gray-900 text-white max-w-lg mx-auto w-full px-4 py-2">
+    Stopka
+  </footer>
+);
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className="max-w-lg mx-auto w-full">
+    <nav className="bg-gray-700 text-white px-4 py-2">Nawigacja</nav>
+  </header>
+);
+
+export default Header;
