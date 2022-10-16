4d330cf5c2ac0ac0cfa4ee9a9a07c9078a6302f4
Pawel <pawel.sioda@gmail.com>
2022-10-16 22:52:58 +0200

    add conditional adding 'selected' class name to links, use enum to pass paths for routes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
+import {useRouter} from "next/router";
+import {AppRoute} from "../../types/AppRoute";
 
-const Header = () => (
-  <header className="max-w-lg mx-auto w-full">
-    <nav className="flex gap-3 bg-gray-700 text-white px-4 py-2">
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-     <Link href="/products">
-         <a>Products</a>
-     </Link>
-    </nav>
-  </header>
-);
+const Header = () => {
+    const router = useRouter();
+
+    return (
+        <header className="max-w-lg mx-auto w-full">
+            <nav className="flex gap-3 bg-gray-700 text-white px-4 py-2">
+                <Link href={AppRoute.home}>
+                    <a className={router.pathname === AppRoute.home ? "selected" : ""}>Home</a>
+                </Link>
+                <Link href={AppRoute.about}>
+                    <a className={router.pathname === AppRoute.about ? "selected" : ""}>About</a>
+                </Link>
+                <Link href={AppRoute.products}>
+                    <a className={router.pathname === AppRoute.products ? "selected" : ""}>Products</a>
+                </Link>
+            </nav>
+        </header>
+    )
+};
 
 export default Header;
