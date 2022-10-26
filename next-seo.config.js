178ed7a77f06caac3a76acbf320f18441a33eeac
Pawel <pawel.sioda@gmail.com>
2022-10-26 22:43:15 +0200

    add default seo config

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+const title = 'My Shop';
+const description = 'My Shop Project from Zaiste Programuj course';
+
+
+const nextSeoConfig = {
+    title,
+    description,
+    openGraph: {
+        url: 'http://my-shop.vercel.app',
+        title,
+        description,
+        siteName: 'My Shop'
+    }
+}
+
+export default nextSeoConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import { AppProps } from "next/app";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import "../styles/globals.css";
 import {Layout} from "../components/Layout";
+import {DefaultSeo} from "next-seo";
+import SEO from '../next-seo.config';
 
 const client = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
       <Layout>
+          <DefaultSeo {...SEO} />
           <QueryClientProvider client={client}>
               <Component {...pageProps} />
           </QueryClientProvider>
